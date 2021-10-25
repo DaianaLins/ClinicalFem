62d69ff57af9ba828328d9e4a1b00f965627ce87
DaianaLins <daiana.lins11@gmail.com>
2021-10-24 21:00:34 -0400

    site com algumas linkagens

---
+++ src/main/java/com/projeto/clinicalfem/models/UsuarioAtendente.java
@@ -0,0 +1,40 @@
+package com.projeto.clinicalfem.models;
+
+public class UsuarioAtendente {
+    private String nome;
+    private String email;
+    private String senha;
+    private String id;
+    private String numero;
+    public String getNome() {
+        return nome;
+    }
+    public String getNumero() {
+        return numero;
+    }
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
