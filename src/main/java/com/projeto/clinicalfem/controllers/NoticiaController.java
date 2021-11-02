a632075c6f22938d60a457e80d5c3b35ab6f68cc
DaianaLins <daiana.lins11@gmail.com>
2021-11-02 10:02:00 -0400

    noticias css

--- src/main/java/com/projeto/clinicalfem/controllers/NoticiaController.java
+++ src/main/java/com/projeto/clinicalfem/controllers/NoticiaController.java
@@ -1,26 +1,18 @@
 package com.projeto.clinicalfem.controllers;
 
 import java.time.LocalDate;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
-import com.google.common.util.concurrent.Service.Listener;
-import com.google.firebase.database.ServerValue;
 import com.projeto.clinicalfem.models.Noticia;
 import com.projeto.clinicalfem.service.NoticiaService;
 
-import org.apache.tomcat.jni.Local;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @RestController
 @RequestMapping("/")
