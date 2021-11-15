72eb71af7bed9ff6564f4bbe7e33ea7d772dee0c
DaianaLins <daiana.lins11@gmail.com>
2021-11-15 11:57:24 -0400

    alguns ajustes

---
+++ src/main/java/com/projeto/clinicalfem/models/UsuarioParse.java
@@ -0,0 +1,14 @@
+package com.projeto.clinicalfem.models;
+
+public class UsuarioParse {
+    public static Usuarios toGoogle(UsuariosSpring antigo){
+        Usuarios novo = new Usuarios(antigo.getId(), antigo.getNome(), (antigo.getImagemLocal() != null ? antigo.getImagemLocal():""), antigo.getTipo(), antigo.isAdm() ,antigo.getDataNascLocal(), antigo.getEmail(), antigo.getSenha(), antigo.getNumero());
+        
+        return novo;
+    }
+
+    public static UsuariosSpring toSpring(Usuarios antigo){
+        UsuariosSpring novo = new UsuariosSpring(antigo.getId(), antigo.getNome(), antigo.getImagem(), antigo.getTipo(), antigo.isAdm(), antigo.getDatanasc(), antigo.getEmail(), antigo.getSenha(), antigo.getNumero());
+        return novo;
+}
+}
